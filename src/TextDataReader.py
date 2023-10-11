04cc26f25648b307f2e3635753678a7a72765cf3
Andrey <xxxtankistxxx@yandex.ru>
2023-10-11 20:50:23 +0300

    Переместили возвращение за пределы цикла

--- src/TextDataReader.py
+++ src/TextDataReader.py
@@ -18,4 +18,6 @@ class TextDataReader(DataReader):
                     subj, score = line.split(":", maxsplit=1)
                     self.students[self.key].append(
                         (subj.strip(), int(score.strip())))
-                    return self.students
+
+        return self.students
+
